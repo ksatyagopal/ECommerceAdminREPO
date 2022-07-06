d08491b3f7c72cb641e5b16234509211540c1470
Satyagopal <dev_satya@outlook.com>
2022-07-06 15:29:14 +0530

    First Commit
    Added AdminDB SQL File

---
+++ AdminDBQueries.sql
@@ -0,0 +1,27 @@
+Create database ECommerceAdminDB
+
+use ECommerceAdminDB
+
+CREATE TABLE Admins(
+	AdminID INT PRIMARY KEY IDENTITY(12345,11),
+	AdminName VARCHAR(50),
+	EMail varchar(50),
+	Mobile bigint,
+	IsSuperAdmin BIT,
+	LastLoggedIn varchar(25),
+	Password VARCHAR(16),
+	IsLoggedIn BIT,
+	IsDeleted BIT,
+	IsLocked BIT,
+	UnSuccessfulAttempts INT
+)
+
+CREATE TABLE Contributions(
+	CID INT PRIMARY KEY IDENTITY(1,1),
+	ChangeMadeBy INT FOREIGN KEY REFERENCES Admins(AdminID),
+	Reference VARCHAR(30),
+	ChangesMade VARCHAR(30),
+	ChangedTime varchar(50),
+	Reason NVARCHAR(MAX)
+)
+
